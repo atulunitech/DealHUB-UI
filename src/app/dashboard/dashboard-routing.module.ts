e26dab9fe83ab0c4dc0c7ca13bfbbfcee01a9285
atulunitech <atulunitech@gmail.com>
2021-04-30 01:45:41 +0530

    creat obf com done

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { CreateOBFComponent } from './dashboard/create-obf/create-obf.component';
+import { CreatobfComponent } from './creatobf/creatobf.component';
 
 const routes: Routes = [
   { path: '', component: DashboardComponent},
   { path: 'createobf', component: CreateOBFComponent},
+  { path: 'Obf', component: CreatobfComponent},
 ];
 
 @NgModule({
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -5,21 +5,24 @@ import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import {MatButtonModule} from '@angular/material/button';
 import {MatTableModule} from '@angular/material/table';
+import {MatExpansionModule} from '@angular/material/expansion';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from '../shared/materialmodule/materialmodule.module';
 import { CreateOBFComponent } from './dashboard/create-obf/create-obf.component';
 import { NgxDropzoneModule } from 'ngx-dropzone';
+import { CreatobfComponent } from './creatobf/creatobf.component';
 
 
 
 @NgModule({
-  declarations: [DashboardComponent, CreateOBFComponent],
+  declarations: [DashboardComponent, CreateOBFComponent, CreatobfComponent],
   imports: [
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
     DashboardRoutingModule,
+    MatExpansionModule,
     MatButtonModule,
     MatTableModule,
     FlexLayoutModule,
