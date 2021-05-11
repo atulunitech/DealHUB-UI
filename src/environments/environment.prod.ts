d507e39e021c24ad2a5f4b9e00a3fc8e967ae3f8
shaileshn86 <naikshailu07@gmail.com>
2021-05-11 15:25:30 +0530

    change for api and error in publish

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://103.81.89.7/DealhubAPI/'
+  apiUrl: 'http://13.235.216.142/dealhubapidev/'
    //apiUrl:'http://localhost:52229/'
 };
