118e3cf34781451d9477a34b6085c607c6f7c16d
shaileshn86 <naikshailu07@gmail.com>
2021-05-11 15:42:45 +0530

    chnage for publish

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:52229/'
+  apiUrl: 'http://13.235.216.142/dealhubapidev/'
  //apiUrl:'http://localhost:52229/'
 };
 
