423cfdb021d9332bf3c8bdfeae6b9c4c902e8a27
shaileshn86 <naikshailu07@gmail.com>
2021-05-26 11:17:00 +0530

    change for publish

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: 'http://13.235.216.142/dealhubapiqa/'
    //apiUrl:'http://localhost:52229/'
-   apiUrl: 'http://13.235.216.142/dealhubapidev/'
+ 
 };
