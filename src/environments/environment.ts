17e01098c44ed80a7bf908da39a2736e69233473
atulunitech <atulunitech@gmail.com>
2021-06-02 15:25:38 +0530

    issues done

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@
 export const environment = {
   production: false,
   // apiUrl: 'http://103.81.89.7/DealhubAPI/'
-  apiUrl:'http://localhost:52229/'
- // apiUrl: 'http://13.235.216.142/dealhubapidev/'
+  // apiUrl:'http://localhost:52229/'
+ apiUrl: 'http://13.235.216.142/dealhubapidev/'
  // apiUrl: 'http://13.235.216.142/dealhubapiqa/'
 };
 
