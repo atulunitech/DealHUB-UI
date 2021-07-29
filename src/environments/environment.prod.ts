35f252d4b9c221ac3aeb1ee262c7f5d6c1447685
Kirti <itsmekeerti.singh@gmail.com>
2021-07-29 18:30:42 +0530

    dashboard services

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,8 +4,8 @@ export const environment = {
  //apiUrl:'http://localhost:52229/'
   //apiUrl: 'http://13.235.216.142/dealhubapidev/'
  // apiUrl: 'http://13.235.216.142/dealhubapiqa/'
-  apiUrl: 'http://13.235.216.142/dealhubapiuat/'
+ // apiUrl: 'http://13.235.216.142/dealhubapiuat/'
  
- // apiUrl: 'http://13.235.216.142/dealhubapivapt/'
+  apiUrl: 'http://13.235.216.142/dealhubapivapt/'
 
 };
