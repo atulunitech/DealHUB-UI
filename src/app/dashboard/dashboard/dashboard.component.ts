77fb37a71574e962d948650d342344f9f0ba800e
Kirti <itsmekeerti.singh@gmail.com>
2021-07-30 17:36:06 +0530

    updated

--- src/app/dashboard/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard/dashboard.component.ts
@@ -1480,6 +1480,7 @@ onchange(evt,solutioncategory)
     });
   
     dialogRef.afterClosed().subscribe(result => {
+      this.dialog.closeAll();
         console.log('The dialog was closed');
         // this.animal = result;
     });
