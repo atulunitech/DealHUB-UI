01aa45aa8c756f6bc52c716010c5c4ce86cf4ef0
Kirti <itsmekeerti.singh@gmail.com>
2021-08-02 13:19:06 +0530

    updated

--- src/app/master/master-listing/master-listing.component.ts
+++ src/app/master/master-listing/master-listing.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-master-listing',
@@ -7,10 +8,32 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class MasterListingComponent implements OnInit {
 @Input() masterType : any;
-  constructor() { }
+  constructor(private router: Router,private route:ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe
+    (params => {
+      if(params['type'] != undefined)
+      {
+        let type = params['type'].toString().trim();
+        this.switchtotypeapi(type);
+      }
+    }
+    );
   }
+  
+  switchtotypeapi(type)
+  {
+     switch (type) {
+       case "":
+         
+         break;
+     
+       default:
+         break;
+     }
+  }
+
   pageTitle()
   {
    return this.masterType;
