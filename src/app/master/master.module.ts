51676f8206aa963f008a880437b0e197dab71999
atul <atulunitech@gmail.com>
2021-08-06 03:52:23 +0530

    user master search check box

--- src/app/master/master.module.ts
+++ src/app/master/master.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule,DatePipe } from '@angular/common';
 
 import { MasterRoutingModule } from './master-routing.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatButtonModule } from '@angular/material/button';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 import { MasterDataComponent } from './master-data/master-data.component';
 import { MasterListingComponent } from './master-listing/master-listing.component';
 import { ProjectTypeComponent } from './project-type/project-type.component';
 import { MaterialModule } from '../shared/materialmodule/materialmodule.module';
 import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { UserMasterComponent } from './user-master/user-master.component';
+import { PerfectScrollbarModule, PerfectScrollbarConfigInterface,
+  PERFECT_SCROLLBAR_CONFIG } from 'ngx-perfect-scrollbar';
+  import { MatMenuModule } from '@angular/material/menu';
+const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
+  wheelPropagation: true
+};
 
 
 
@@ -18,9 +26,18 @@ import { UserMasterComponent } from './user-master/user-master.component';
     CommonModule,
     FlexLayoutModule,
     MasterRoutingModule,
+    MatButtonModule,
+    MatCheckboxModule,
     MaterialModule,
+    PerfectScrollbarModule,
     ReactiveFormsModule,
-    FormsModule
-  ]
+    FormsModule,
+    MatMenuModule
+  ],
+  providers:[DatePipe,
+    {
+      provide: PERFECT_SCROLLBAR_CONFIG,
+      useValue: DEFAULT_PERFECT_SCROLLBAR_CONFIG
+    }]
 })
 export class MasterModule { }
