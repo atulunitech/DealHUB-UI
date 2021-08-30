55a0a5f711931ad1a333cdc6697d49b22524d1ac
ankita <ankita.aherkar96@gmail.com>
2021-08-30 19:04:01 +0530

    hanges

--- src/app/master/master-listing/master-listing.component.ts
+++ src/app/master/master-listing/master-listing.component.ts
@@ -119,7 +119,7 @@ dontshowforDOA:boolean = true;
       // Validators.email,
       Role_code : new FormControl('', [Validators.required,this.NoInvalidCharacters,this.noWhitespaceValidator]),
       role_name:new FormControl('', [Validators.required,this.NoInvalidCharacters,this.noWhitespaceValidator]),
-      equivalent_cassh_role_name:new FormControl('', [this.NoInvalidCharacters,this.noWhitespaceValidator]),
+      equivalent_cassh_role_name:new FormControl('', [Validators.required,this.NoInvalidCharacters,this.noWhitespaceValidator]),
       privilege:new FormControl('', [Validators.required]),
       Rolestatus:new FormControl("", [Validators.required])
     });
