127c419cc61052a80217141f4b9b62e920625cda
Kirti <itsmekeerti.singh@gmail.com>
2021-08-31 17:50:33 +0530

    updated

--- src/app/dashboard/creatobf/creatobf.component.ts
+++ src/app/dashboard/creatobf/creatobf.component.ts
@@ -438,11 +438,12 @@ export class CreatobfComponent implements OnInit {
       }
       this.servicesControl.setValue(this._obfservices.servicesarray);
       this._obfservices.ObfCreateForm.patchValue({Otherservicesandcategories:this._obfservices.servicesarray});
-
+     //added on 31-Aug-2021 for unique sector
       let ressec = this.sectorlist.filter(obj =>{
         return obj.vertical_id === this._obfservices.editObfObject._vertical_id;
       });
       this.sectorlist = <sectors[]>ressec;
+      //end
       var resultnew = this.subsectorlist.filter(obj => {
         return obj.Sector_Id === this._obfservices.editObfObject._Sector_Id;
       });
