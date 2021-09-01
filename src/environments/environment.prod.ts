76b67c9319464ea9b5597c9c98b6a118aae35c1f
shaileshn86 <naikshailu07@gmail.com>
2021-09-01 09:51:00 +0530

    change for deployments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: true,
  // apiUrl: 'http://103.81.89.7/DealhubAPI/'
  //apiUrl:'http://localhost:52229/'
-  //apiUrl: 'http://13.235.216.142/dealhubapiqa'
-  apiUrl:'http://10.175.17.48/dealhubapiqa/' 
+  apiUrl: 'http://13.235.216.142/dealhubapiqa/'
+  //apiUrl:'http://10.175.17.48/dealhubapi/' 
   // apiUrl: 'http://13.235.216.142/dealhubapiuat/'
   //apiUrl: 'http://10.175.17.48/dealhubapi/'
   //apiUrl: 'http://13.235.216.142/dealhubapivapt/'
